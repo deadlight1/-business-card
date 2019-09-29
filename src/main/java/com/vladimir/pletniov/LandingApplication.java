57cee9cddefc3c49424cbb5e0c5c19bddb65e009
vova967 <w.pletniov@ua.head.com>
2019-09-30 00:05:04 +0300

    Little refactor

--- src/main/java/com/vladimir/pletniov/LandingApplication.java
+++ src/main/java/com/vladimir/pletniov/LandingApplication.java
@@ -1,4 +1,4 @@
-package com.example.landing;
+package com.vladimir.pletniov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/vladimir/pletniov/LandingApplicationTests.java
+++ src/test/java/com/vladimir/pletniov/LandingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.landing;
+package com.vladimir.pletniov;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
